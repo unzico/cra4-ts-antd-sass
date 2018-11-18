b3a3bbda8cf963ee643f810a39df8582cb1d6455
Florian U <unzico@gmail.com>
2018-11-18 04:05:19 +0100

    Support absolute import paths

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,12 @@
+const path = require("path");
+const fs = require("fs");
 const CracoAntDesignPlugin = require("craco-antd");
 
 module.exports = {
-  plugins: [{ plugin: CracoAntDesignPlugin }]
+  plugins: [{ plugin: CracoAntDesignPlugin }],
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src/")
+    }
+  }
 };
---
+++ src/AbsolutePathComponent.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+type Props = {};
+
+const AbsolutePathComponent: React.SFC<Props> = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default AbsolutePathComponent;
